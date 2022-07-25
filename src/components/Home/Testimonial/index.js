e2d9510805e9cb89a86a7eef929e1706721b35e8
yosuaindra <yosua.is.dev@gmail.com>
2022-07-26 01:04:15 +0700

    update slider testimonial

--- src/components/Home/Testimonial/index.js
+++ src/components/Home/Testimonial/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Container, Row, Card } from 'reactstrap';
 import './style.scss';
 import Slider from "react-slick";
@@ -5,15 +6,36 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const Testimonial = () => {
+    const slider = React.useRef();
+    
     const settings = {
         speed: 500,
-        className: "center",
-        centerMode: true,
-        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
-        // variableWidth: true,
-        autoplay: true
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
 
     return (
@@ -27,7 +49,7 @@ const Testimonial = () => {
                         </div>
                     </div>
                     <div className='wrapper-slider'>
-                            <Slider {...settings}>
+                            <Slider ref={slider} {...settings}>
                                 <Card>
                                     <div className='image col-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-center'>
                                         <img src='https://cdn.discordapp.com/attachments/986612883859341333/997421101493780590/unknown.png' alt='testimonial' />
@@ -62,23 +84,7 @@ const Testimonial = () => {
                                 </Card>
                                 <Card>
                                     <div className='image col-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-center'>
-                                        <img src='https://cdn.discordapp.com/attachments/986612883859341333/997421148054753320/unknown.png' alt='testimonial' />
-                                    </div>
-                                    <div className='desc col-12 col-md-8 col-lg-8 d-flex justify-content-center'>
-                                        <div className='rating'>
-                                            <span className='fa fa-star checked'></span>
-                                            <span className='fa fa-star checked'></span>
-                                            <span className='fa fa-star checked'></span>
-                                            <span className='fa fa-star checked'></span>
-                                            <span className='fa fa-star checked'></span>
-                                        </div>
-                                        <h3>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod&rdquo;</h3>
-                                        <p>John Dee 32, Bromo</p>
-                                    </div>
-                                </Card>
-                                <Card>
-                                    <div className='image col-12 col-md-4 col-lg-4 d-flex justify-content-center align-items-center'>
-                                        <img src='https://cdn.discordapp.com/attachments/986612883859341333/997421148054753320/unknown.png' alt='testimonial' />
+                                        <img src='https://cdn.discordapp.com/attachments/986612883859341333/997421101493780590/unknown.png' alt='testimonial' />
                                     </div>
                                     <div className='desc col-12 col-md-8 col-lg-8 d-flex justify-content-center'>
                                         <div className='rating'>
@@ -111,8 +117,8 @@ const Testimonial = () => {
                             </Slider>
                             
                             <div className='nav-slider d-flex justify-content-center'>
-                                <span className='fa-solid fa-angle-left'></span>
-                                <span className='fa-solid fa-angle-right'></span>
+                                <span onClick={() => slider.current.slickPrev()} className='fa-solid fa-angle-left'></span>
+                                <span onClick={() => slider.current.slickNext()} className='fa-solid fa-angle-right'></span>
                             </div>
                         </div>
                 </Row>
